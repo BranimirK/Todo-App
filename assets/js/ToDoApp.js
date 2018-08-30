96ff8f30c496deb89b4e129e2dcf5ecf8290acff
BranimirK <bkedacic@hotmail.com>
2018-08-30 18:00:14 +0200

    styling, toggle, fade
    styling
    toggle add new todo
    fade delete

--- assets/js/ToDoApp.js
+++ assets/js/ToDoApp.js
@@ -18,9 +18,12 @@ $("input[type='text']").keypress(function(keyEvent){
 		//grab a new todo text from input
 		var todoText = $(this).val();
 		//create and add a new li
-		$("ul").prepend("<li><span>X</span> " +todoText +"</ul>")
+		$("ul").prepend("<li><span><i class='far fa-trash-alt'></i></span> " +todoText +"</ul>")
 		//delete text in input box
 		$(this).val("");
-
 	}
 });
+
+$(".fa-edit").click(function(){
+	$("input[type='text']").fadeToggle();
+})
